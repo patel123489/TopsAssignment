ed5cc08929e4bd20653c963646260c5ec5b9b44d
om Patel <omviralpatel90106@gmail.com>
2025-08-19 12:54:33 +0530

    Patel 08

--- Assigments/Module 3 lab exercise/Q-3. Control Flow Statements/1.Grade-Calculator.cpp
+++ Assigments/Module 3 lab exercise/Q-3. Control Flow Statements/1.Grade-Calculator.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
 using namespace std;
-main(){
+main() {
     int marks;
     cout << "Enter marks (0-100): ";
     cin >> marks;
+
     if (marks >= 90 && marks <= 100) {
-        cout << "Grade: A+" << endl;
+        cout << "Grade: A+";
     }
     else if (marks >= 80) {
-        cout << "Grade: A" << endl;
+        cout << "Grade: A";
     }
     else if (marks >= 70) {
-        cout << "Grade: B" << endl;
+        cout << "Grade: B";
     }
     else if (marks >= 60) {
-        cout << "Grade: C" << endl;
+        cout << "Grade: C";
     }
     else if (marks >= 50) {
-        cout << "Grade: D" << endl;
+        cout << "Grade: D";
     }
     else if (marks >= 0) {
-        cout << "Grade: Fail" << endl;
+        cout << "Grade: Fail";
     }
     else {
-        cout << "Invalid marks entered!" << endl;
+        cout << "Invalid marks entered!";
     }
-
 }
